272401448b521e044ecc279d561f5949bda89768
hudae <hudaejaz219@gmail.com>
2023-04-26 00:57:57 +0400

    product list element location and text verification

--- cypress/e2e/0-conduetTests/AmazonFeatureTest.spec.cy.js
+++ cypress/e2e/0-conduetTests/AmazonFeatureTest.spec.cy.js
@@ -1,30 +1,32 @@
 /// <reference types = "cypress" />
 /// <reference types="cypress-xpath" />
-require('cypress-xpath') 
+require('cypress-xpath')
 
 
-describe("Test category selection of Amazon",()=>{
+describe("Test category selection of Amazon", () => {
 
 
-    it("open Amazon and select category",()=>{
+    it("open Amazon and select category", () => {
 
-    
-    cy.visit("https://www.amazon.ae")
-    
-    cy.get("#searchDropdownBox").select("Electronics",{force:true})
-    
-    //cy.get("#twotabsearchtextbox").type("apple watch")
 
-   cy.get("#nav-search-submit-button").click()
+        cy.visit("https://www.amazon.ae")
 
-   cy.xpath("//a/span[contains(text(),'Earbuds & Accessories')]").click()
+        cy.get("#searchDropdownBox").select("Electronics", { force: true })
 
-    cy.get('[data-component-type="s-product-image"]').as("products")
+        //cy.get("#twotabsearchtextbox").type("apple watch")
 
-    cy.get("@products").eq(4).invoke('text').then(productsText => {
+        cy.get("#nav-search-submit-button").click()
 
-        cy.log(productsText)
-    })
+        cy.xpath("//a/span[contains(text(),'Earbuds & Accessories')]").click()
+
+        cy.get('[class="_octopus-search-result-card_style_apbSearchResultItem__2-mx4"]').as("products")
+
+        cy.get("@products").eq(0).invoke('text').then(productsText => {
+
+            cy.log(productsText)
+            expect(productsText).contains("New Apple AirPods Pro (2nd Gen)")
+            
+        })
 
     })
 
